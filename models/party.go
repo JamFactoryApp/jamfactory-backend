df7edc6eafe5cd7b697c665781d9e9123bb0331d
Tim <xmrtimex@gmail.com>
2020-05-07 21:29:30 +0200

    :construction: Add PartyController to global environment

--- models/party.go
+++ models/party.go
@@ -10,13 +10,13 @@ type Party struct {
 	partyLabel string
 	spotifyClient spotify.Client
 	queue Queue
-	socket socketio.Server
-	currentSong spotify.FullTrack
+	socket *socketio.Server
+	currentSong *spotify.FullTrack
 	queueActive bool
 	selectedDeviceId spotify.ID
 	ipVoting bool
-	playbackState spotify.PlayerState
-	user spotify.User
+	playbackState *spotify.PlayerState
+	user *spotify.User
 }
 
 type PartySettings struct {
@@ -24,12 +24,12 @@ type PartySettings struct {
 	IpVoting bool `json:"ip"`
 }
 
-func (party *Party) setUser (user spotify.User) {
+func (party *Party) setUser (user *spotify.User) {
 	party.user = user
 	party.user.DisplayName = strings.Join([]string{party.user.DisplayName, "'s Jam Session"}, "")
 }
 
-func (party *Party) getUser () spotify.User {
+func (party *Party) getUser () *spotify.User {
 	return party.user
 }
 
@@ -64,15 +64,15 @@ func (party *Party) setSetting (setting PartySettings) {
 	party.ipVoting = setting.IpVoting
 }
 
-func (party *Party)  getCurrentSong() spotify.FullTrack{
+func (party *Party)  getCurrentSong() *spotify.FullTrack{
 	return party.currentSong
 }
 
 func (party *Party) getPlaybackState() *spotify.PlayerState{
-	return &party.playbackState
+	return party.playbackState
 }
 
-func (party *Party) setPlaybackState(playbackState spotify.PlayerState) {
+func (party *Party) setPlaybackState(playbackState *spotify.PlayerState) {
 	party.playbackState = playbackState
 }
 
@@ -97,7 +97,7 @@ func (party *Party) setQueueActive(state bool) {
 }
 
 func (party *Party) startNextSong() {
-	party.currentSong = party.queue.GetNextSong(true).Song //Will fail if queue is empty
+	party.currentSong = &party.queue.GetNextSong(true).Song //Will fail if queue is empty
 
 	playOptions := spotify.PlayOptions{
 		URIs:            []spotify.URI{party.getCurrentSong().URI},
