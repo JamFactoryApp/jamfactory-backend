1769384d55ee0f58b3c702444658b88af1a52933
Tim <xmrtimex@gmail.com>
2020-05-09 01:42:51 +0200

    :construction: Remove session example

--- controller/spotify.go
+++ controller/spotify.go
@@ -1,9 +1,7 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 )
 
@@ -16,39 +14,7 @@ func RegisterSpotifyRoutes(router *mux.Router) {
 }
 
 func devices(w http.ResponseWriter, r *http.Request) {
-	session, err := Store.Get(r, "user-session")
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if session.IsNew {
-		session.Values["visits"] = int32(0)
-		err = session.Save(r, w)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}
-
-	session.Values["visits"] = session.Values["visits"].(int32) + 1
-	err = session.Save(r, w)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if session.IsNew {
-		_, err := fmt.Fprintf(w, "This is the first time you visited this site! Welcome")
-		if err != nil {
-			log.Println("Error printing to http.ResponseWriter")
-		}
-	}
-
-	_, err = fmt.Fprintf(w, "You visited this site %d times ", session.Values["visits"].(int32))
-	if err != nil {
-		log.Println("Error printing to http.ResponseWriter")
-	}
 }
 
 func pause(w http.ResponseWriter, r *http.Request) {
