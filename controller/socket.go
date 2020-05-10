ef65a3f951ddfbfe7bbe07618e1175ee403f35e3
Tim <xmrtimex@gmail.com>
2020-05-10 13:16:56 +0200

    :construction: SocketAuth handler now works as intended; Add handlers for error and disconnect

--- controller/socket.go
+++ controller/socket.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"errors"
+	"fmt"
 	socketio "github.com/googollee/go-socket.io"
 	"log"
 	"net/http"
@@ -9,8 +10,8 @@ import (
 
 // Initialize socketio server
 func InitSocketIO() *socketio.Server {
-	socket, err := socketio.NewServer(nil)
 
+	socket, err := socketio.NewServer(nil)
 	if err != nil {
 		log.Fatalln("Error creating socketio server\n", err)
 	}
@@ -23,22 +24,52 @@ func InitSocketIO() *socketio.Server {
 func RegisterSocketRoutes(socket *socketio.Server) {
 	socket.OnConnect("/", SocketAuth)
 	socket.OnEvent("/", "connection", SocketConnect)
+	socket.OnError("/", SocketError)
+	socket.OnDisconnect("/", SocketDisconnect)
 }
 
 func SocketConnect(s socketio.Conn, msg string) {
 	session, _ := Store.Get(&http.Request{Header: s.RemoteHeader()}, "user-session")
+	log.Printf("Socket.Io connection from %s", session.ID)
 
-	s.Join(session.Values["label"].(string))
+	log.Printf(msg)
+	//s.Join(session.Values["label"].(string))
+	s.Join(msg)
 }
 
 func SocketAuth(s socketio.Conn) error {
-	session, _ := Store.Get(&http.Request{Header: s.RemoteHeader()}, "user-session")
-	if session.Values["user"] == "Host" || session.Values["user"] == "Guest" {
-		if PartyControl.GetParty(session.Values["label"].(string)) != nil {
+
+	log.Printf("Start Socket.Io auth")
+
+	session, err := Store.Get(&http.Request{Header: s.RemoteHeader()}, "user-session")
+
+	if err != nil {
+		s.Close()
+		log.Printf("Could not get session")
+		return err
+	}
+
+	s.LeaveAll()
+	log.Printf("Socket.Io auth from %s", session.ID)
+	if (session.Values["User"] == "Host" || session.Values["User"] == "Guest") && session.Values["Label"] != nil {
+		if PartyControl.GetParty(session.Values["Label"].(string)) != nil {
+			log.Printf("allowed")
+			s.Join(session.Values["Label"].(string))
+			s.SetContext(s.Context())
 			return nil
 		} else {
+			s.Close()
 			return errors.New("label is invalid")
 		}
 	}
+	s.Close()
 	return errors.New("wrong user type")
+}
+
+func SocketError(s socketio.Conn, e error) {
+	fmt.Println("meet error:", e)
+}
+
+func SocketDisconnect(s socketio.Conn, reason string) {
+	fmt.Println("closed", reason)
 }
