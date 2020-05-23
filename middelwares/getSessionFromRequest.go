a47d8baf4d6eb5a6391e8df8d3f6c7225ca44c5e
Tim <xmrtimex@gmail.com>
2020-05-23 18:18:31 +0200

    :building_construction: Introduce middleware to get session from request

---
+++ middelwares/getSessionFromRequest.go
@@ -0,0 +1,29 @@
+package middelwares
+
+import (
+	"context"
+	log "github.com/sirupsen/logrus"
+	"jamfactory-backend/models"
+	"net/http"
+)
+
+
+
+type GetSessionFromRequest struct {
+	Store *models.SessionStore
+}
+
+func (middleware *GetSessionFromRequest) Handler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		session, err := middleware.Store.Get(r, "user-session")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Debug("Could not get session for request")
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "Session", session)
+		rWithCtx := r.WithContext(ctx)
+		next.ServeHTTP(w, rWithCtx)
+	})
+}
