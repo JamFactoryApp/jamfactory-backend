4518f8ac03e53309d7e2466d9800b9cae9b677d3
Tim <xmrtimex@gmail.com>
2020-05-24 15:55:32 +0200

    :recycle: Cleanup code and use new helpers and middlewares

--- controller/auth.go
+++ controller/auth.go
@@ -1,10 +1,13 @@
 package controller
 
 import (
-	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	chain "github.com/justinas/alice"
 	log "github.com/sirupsen/logrus"
 	"github.com/zmb3/spotify"
+	"jamfactory-backend/helpers"
+	"jamfactory-backend/middelwares"
 	"net/http"
 	"os"
 )
@@ -12,23 +15,20 @@ import (
 var SpotifyAuthenticator spotify.Authenticator
 
 func RegisterAuthRoutes(router *mux.Router) {
+	getSessionMiddleware := middelwares.GetSessionFromRequest{Store: Store}
+	stdChain := chain.New(getSessionMiddleware.Handler)
 	SpotifyAuthenticator.SetAuthInfo(os.Getenv("SPOTIFY_ID"), os.Getenv("SPOTIFY_SECRET"))
 
-	router.HandleFunc("/callback", callback)
-	router.HandleFunc("/login/", login)
-	router.HandleFunc("/logout", logout)
-	router.HandleFunc("/status/", status)
+	router.Handle("/callback", stdChain.ThenFunc(callback))
+	router.Handle("/login/", stdChain.ThenFunc(login))
+	router.Handle("/logout", stdChain.ThenFunc(logout))
+	router.Handle("/status/", stdChain.ThenFunc(status))
 }
 
 func callback(w http.ResponseWriter, r *http.Request) {
 	log.Trace("Controller call: auth.callback")
 
-	session, err := Store.Get(r, "user-session")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Error("Could not get session: ", err.Error())
-		return
-	}
+	session := r.Context().Value("Session").(*sessions.Session)
 
 	token, err := SpotifyAuthenticator.Token(session.ID, r)
 
@@ -49,32 +49,18 @@ func callback(w http.ResponseWriter, r *http.Request) {
 
 	session.Values["Token"] = token
 	session.Values["User"] = "Host"
-	err = session.Save(r, w)
 
-	if err != nil {
-		log.WithField("Session", session.ID).Error("Couldn't save session: ", err.Error())
-	}
-
+	helpers.SaveSession(w, r, session)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
 	log.Trace("Controller call: auth.login")
 
-	session, err := Store.Get(r, "user-session")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Error("Could not get session", err.Error())
-		return
-	}
+	session := r.Context().Value("Session").(*sessions.Session)
 
 	if session.IsNew {
-		err = session.Save(r, w)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.WithField("Session", session.ID).Error("Could not save session", err.Error())
-			return
-		}
+		helpers.SaveSession(w, r, session)
 	}
 
 	state := session.ID
@@ -83,78 +69,37 @@ func login(w http.ResponseWriter, r *http.Request) {
 	res := make(map[string]interface{})
 	res["url"] = url
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	err = json.NewEncoder(w).Encode(res)
-
-	if err != nil {
-		log.Error("Couldn't encode json: ", err.Error())
-	}
+	helpers.RespondWithJSON(w, res)
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
 	log.Trace("Controller call: auth.logout")
 
-	session, err := Store.Get(r, "user-session")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Error("Could not get session: ", err.Error())
-		return
-	}
+	session := r.Context().Value("Session").(*sessions.Session)
 
 	session.Options.MaxAge = -1
-	err = session.Save(r, w)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.WithField("Session", session.ID).Error("Could not save session", err.Error())
-		return
-	}
-
+	helpers.SaveSession(w, r, session)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func status(w http.ResponseWriter, r *http.Request) {
 	log.Trace("Controller call: auth.status")
 
-	session, err := Store.Get(r, "user-session")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Error("Could not get session: ", err.Error())
-		return
-	}
+	session := r.Context().Value("Session").(*sessions.Session)
 
 	res := make(map[string]interface{})
 
 	if session.Values["User"] == nil {
 		res["user"] = "New"
+	} else {
+		res["user"] = session.Values["User"]
+	}
+
+	if session.Values["Label"] == nil {
 		res["label"] = ""
 	} else {
-		if session.Values["User"] == "Guest" {
-			res["user"] = "Guest"
-			if session.Values["Label"] == nil {
-				res["label"] = ""
-			} else {
-				res["label"] = session.Values["Label"].(string)
-			}
-		} else if session.Values["User"] == "Host" {
-			res["user"] = "Host"
-			if session.Values["Label"] == nil {
-				res["label"] = ""
-			} else {
-				res["label"] = session.Values["Label"].(string)
-			}
-		} else {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-			return
-		}
+		res["label"] = session.Values["Label"]
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	err = json.NewEncoder(w).Encode(res)
-
-	if err != nil {
-		log.Error("Couldn't encode json: ", err.Error())
-	}
+	helpers.RespondWithJSON(w, res)
 }
