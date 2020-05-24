62ad38584e78e35eca5ae7380b80de76a38481c5
Tim <xmrtimex@gmail.com>
2020-05-24 12:52:41 +0200

    :bug: Compare wanted and current user type correctly

--- middelwares/validateUserType.go
+++ middelwares/validateUserType.go
@@ -14,7 +14,7 @@ func (middleware *ValidateUserType) Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		session := r.Context().Value("Session").(*sessions.Session)
 
-		if !(session.Values["User"] != "Host") {
+		if !(session.Values["User"] == "Host") {
 			http.Error(w, "User Error: Not the correct user", http.StatusUnauthorized)
 			log.WithFields(log.Fields{
 				"Current": session.Values["User"],
