230ff99d138aad87b0c0a316646c34ee05e53025
Jonas Levin Weber <DrJoLe@users.noreply.github.com>
2020-05-25 18:37:00 +0200

    :recycle: Group routes and middlewares

---
+++ controller/middlewares.go
@@ -0,0 +1,88 @@
+package controller
+
+import (
+	"context"
+	log "github.com/sirupsen/logrus"
+	"jamfactory-backend/utils"
+	"net/http"
+)
+
+type Middleware interface {
+	Handler(http.Handler) http.Handler
+}
+
+type PartyRequiredMiddleware struct{}
+
+type SessionRequiredMiddleware struct{}
+
+type UserTypeRequiredMiddleware struct {
+	UserType string
+}
+
+func (*PartyRequiredMiddleware) Handler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session := utils.SessionFromRequestContext(r)
+
+		if session == nil {
+			http.Error(w, "", http.StatusInternalServerError)
+			log.Panic("Could not get session from context")
+			return
+		}
+
+		logger := log.WithField("Session", session.ID)
+
+		if !(session.Values[SessionLabelKey] != nil) {
+			http.Error(w, "User error: Not joined a party", http.StatusUnauthorized)
+			logger.Trace("Could not get party: User not joined a party")
+			return
+		}
+
+		party := GetParty(session.Values[SessionLabelKey].(string))
+
+		if party == nil {
+			http.Error(w, "Party error: Could not find a party with the submitted label", http.StatusNotFound)
+			logger.WithField("Label", session.Values[SessionLabelKey].(string)).Trace("Could not get party: Party not found")
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), utils.PartyContextKey, party)
+		rWithCtx := r.WithContext(ctx)
+
+		next.ServeHTTP(w, rWithCtx)
+	})
+}
+
+func (*SessionRequiredMiddleware) Handler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session, err := GetSession(r, "user-session")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Debug("Could not get session for request")
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), utils.SessionContextKey, session)
+		rWithCtx := r.WithContext(ctx)
+
+		next.ServeHTTP(w, rWithCtx)
+	})
+}
+
+func (m *UserTypeRequiredMiddleware) Handler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session := utils.SessionFromRequestContext(r)
+
+		if !(session.Values[SessionUserTypeKey] == m.UserType) {
+			http.Error(w, "User Error: Not the correct user type", http.StatusUnauthorized)
+			log.WithFields(log.Fields{
+				"Current": session.Values[SessionUserTypeKey],
+				"Wanted":  m.UserType,
+				"Session": session.ID,
+			}).Debug("User Error: Not the correct user type")
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
---
+++ controller/routes.go
@@ -0,0 +1,119 @@
+package controller
+
+import (
+	"github.com/googollee/go-socket.io"
+	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
+	"jamfactory-backend/models"
+)
+
+const (
+	apiPath = "/api"
+
+	authPath         = "/auth"
+	authCallbackPath = "/callback"
+	authLoginPath    = "/login/"
+	authLogoutPath   = "/logout"
+	authStatusPath   = "/status/"
+
+	partyPath         = "/party"
+	partyIndexPath    = "/"
+	partyCreatePath   = "/create"
+	partyJoinPath     = "/join"
+	partyLeavePath    = "/leave"
+	partyPlaybackPath = "/playback"
+
+	queuePath         = "/queue"
+	queueIndexPath    = "/"
+	queuePlaylistPath = "/playlist"
+	queueVotePath     = "/vote"
+
+	spotifyPath         = "/spotify"
+	spotifyDevicesPath  = "/devices"
+	spotifyPlaylistPath = "/playlist"
+	spotifySearchPath   = "/search"
+
+	socketIOPath    = "/socket.io/"
+	socketIndexPath = "/"
+)
+
+var (
+	Router *mux.Router
+	Socket *socketio.Server
+
+	authRouter    *mux.Router
+	partyRouter   *mux.Router
+	queueRouter   *mux.Router
+	spotifyRouter *mux.Router
+
+	socketRouter *mux.Router
+
+	sessionRequiredMiddleware = SessionRequiredMiddleware{}
+	partyRequiredMiddleware   = PartyRequiredMiddleware{}
+	hostRequiredMiddleware    = UserTypeRequiredMiddleware{UserType: models.UserTypeHost}
+
+	sessionRequired alice.Chain
+	partyRequired   alice.Chain
+	hostRequired    alice.Chain
+)
+
+func initRoutes() {
+	Router = mux.NewRouter()
+
+	authRouter = Router.PathPrefix(apiPath + authPath).Subrouter()
+	partyRouter = Router.PathPrefix(apiPath + partyPath).Subrouter()
+	queueRouter = Router.PathPrefix(apiPath + queuePath).Subrouter()
+	spotifyRouter = Router.PathPrefix(apiPath + spotifyPath).Subrouter()
+
+	socketRouter = Router.PathPrefix(socketIOPath).Subrouter()
+
+	registerAuthRoutes()
+	registerQueueRoutes()
+	registerPartyRoutes()
+	registerSpotifyRoutes()
+
+	registerSocketIORoutes()
+}
+
+func initMiddleWares() {
+	sessionRequired = alice.New(sessionRequiredMiddleware.Handler)
+	partyRequired = sessionRequired.Append(partyRequiredMiddleware.Handler)
+	hostRequired = partyRequired.Append(hostRequiredMiddleware.Handler)
+}
+
+func registerAuthRoutes() {
+	authRouter.Handle(authCallbackPath, sessionRequired.ThenFunc(callback)).Methods("GET")
+	authRouter.Handle(authLoginPath, sessionRequired.ThenFunc(login)).Methods("GET")
+	authRouter.Handle(authLogoutPath, sessionRequired.ThenFunc(logout)).Methods("GET")
+	authRouter.Handle(authStatusPath, sessionRequired.ThenFunc(status)).Methods("GET")
+}
+
+func registerPartyRoutes() {
+	partyRouter.Handle(partyCreatePath, sessionRequired.ThenFunc(createParty)).Methods("GET")
+	partyRouter.Handle(partyJoinPath, partyRequired.ThenFunc(joinParty)).Methods("PUT")
+	partyRouter.Handle(partyLeavePath, partyRequired.ThenFunc(leaveParty)).Methods("GET")
+	partyRouter.Handle(partyIndexPath, partyRequired.ThenFunc(getParty)).Methods("GET")
+	partyRouter.Handle(partyIndexPath, hostRequired.ThenFunc(setParty)).Methods("PUT")
+	partyRouter.Handle(partyPlaybackPath, partyRequired.ThenFunc(getPlayback)).Methods("GET")
+	partyRouter.Handle(partyPlaybackPath, hostRequired.ThenFunc(setPlayback)).Methods("PUT")
+}
+
+func registerQueueRoutes() {
+	queueRouter.Handle(queueIndexPath, partyRequired.ThenFunc(getQueue)).Methods("GET")
+	queueRouter.Handle(queuePlaylistPath, partyRequired.ThenFunc(addPlaylist)).Methods("PUT")
+	queueRouter.Handle(queueVotePath, partyRequired.ThenFunc(vote)).Methods("PUT")
+}
+
+func registerSpotifyRoutes() {
+	spotifyRouter.Handle(spotifyDevicesPath, partyRequired.ThenFunc(devices)).Methods("GET")
+	spotifyRouter.Handle(spotifyPlaylistPath, partyRequired.ThenFunc(playlist)).Methods("GET")
+	spotifyRouter.Handle(spotifySearchPath, partyRequired.ThenFunc(search)).Methods("PUT")
+}
+
+func registerSocketIORoutes() {
+	Socket.OnConnect(socketIndexPath, socketIOConnect)
+	Socket.OnError(socketIndexPath, socketIOError)
+	Socket.OnDisconnect(socketIndexPath, socketIODisconnect)
+
+	socketRouter.Handle(socketIndexPath, Socket)
+}
