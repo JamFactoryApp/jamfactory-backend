10db2ea24152f1995534e64ba20c2ae223c78b90
Jonas Levin Weber <drjole@users.noreply.github.com>
2020-10-01 19:30:30 +0200

    :goal_net: Allow empty passwords for redis

--- models/db.go
+++ models/db.go
@@ -23,9 +23,11 @@ func newRedisPool() *redis.Pool {
 			if err != nil {
 				return nil, err
 			}
-			if _, err := c.Do("AUTH", os.Getenv("REDIS_PASSWORD")); err != nil {
-				_ = c.Close()
-				return nil, err
+			if password, ok := os.LookupEnv("REDIS_PASSWORD"); ok {
+				if _, err := c.Do("AUTH", password); err != nil {
+					_ = c.Close()
+					return nil, err
+				}
 			}
 			if _, err := c.Do("SELECT", os.Getenv("REDIS_DATABASE")); err != nil {
 				_ = c.Close()
