c9c76bb062a8f9d220a628ceb6e9d966e7c6299e
Tim <xmrtimex@gmail.com>
2020-10-06 21:00:08 +0200

    :art: Update Playback even if JamSession is not active

--- controllers/jamfactory.go
+++ controllers/jamfactory.go
@@ -123,11 +123,12 @@ func Conductor(jamSession *models.JamSession) {
 					jamSession.StartNextSong()
 					SendToRoom(jamSession.Label, SocketEventQueue, jamSession.Queue.GetObjectWithoutId(""))
 				}
-				res := types.SocketPlaybackState{
-					Playback: *jamSession.PlaybackState,
-				}
-				SendToRoom(jamSession.Label, SocketEventPlayback, res)
 			}
+
+			res := types.SocketPlaybackState{
+				Playback: *jamSession.PlaybackState,
+			}
+			SendToRoom(jamSession.Label, SocketEventPlayback, res)
 		}
 	}
 }
