d6989a66925eb4d854a9226011e86fbb8802ceb8
Jonas Levin Weber <DrJoLe@users.noreply.github.com>
2020-10-08 19:13:29 +0200

    :wrench: Require protocol scheme for JamFactory client address

--- utils/env.go
+++ utils/env.go
@@ -5,10 +5,14 @@ import (
 	log "github.com/sirupsen/logrus"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func JamClientAddress() string {
 	address := os.Getenv("JAM_CLIENT_ADDRESS")
+	if !strings.HasPrefix(address, "http") {
+		log.Fatal("JAM_CLIENT_ADDRESS requires protocol scheme (e.g. http://)")
+	}
 	port, err := strconv.Atoi(os.Getenv("JAM_CLIENT_PORT"))
 	if err != nil {
 		log.Fatal("Invalid client port")
